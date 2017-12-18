4d4d673f9ba5e15238682699c38855e6e7626a33
yangliu-nokia <yang.7.liu@nokia.com>
2017-12-18 14:46:07 +0100

    Initial Commit

---
+++ WB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WB
+//
+//  Created by Liu, Yang 7. (Nokia - FR/Nozay) on 18/12/2017.
+//  Copyright © 2017 Liu, Yang 7. (Nokia - FR/Nozay). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
